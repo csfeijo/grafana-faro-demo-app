743e70602579c0bfdf3277ac49c0c4f6e63275a3
Cícero Feijó <cicero.feijo@gmail.com>
2024-08-04 19:17:34 -0300

    feat: adiciona arquivos de demo

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# grafana-faro-demo-app
+# Grafana Faro: levando a observabilidade para o frontend
+
+## Projeto DEMO
+
+Aplicação para demonstração e consulta sobre a palestra ministrada por mim no [TDC Florianópolis](https://thedevconf.com/tdc/2024/florianopolis/trilha-web-e-front-end) em 2024.
+
+Baseado no material do vídeo [Mastering Frontend Observability in React with Grafana Faro](https://www.youtube.com/watch?v=IA_-zkpVhIU).
+
+Para acesso a dashboard você precisa ter configurado o seu projeto na [Grafana Cloud](https://grafana.com/auth/sign-in/).
+
+![Dashboard](docs/dashboard.png)
+
+## Requisitos
+
+- Node18 >=
+- React
+- Vite
+- pnpm
+- crie uma conta em [Grafana Cloud](https://grafana.com/auth/sign-in/)
+
+## Execução
+
+Cumprindo os requisitos acima execute:
+
+```
+pnpm install
+pnpm run dev
+```
+
+O servidor será iniciado em `http://localhost:5173`
+
+## Dúvidas
+
+Me contate se houver necessidade e use o conteúdo desse repositório como desejar.
+
+[Professor Feijó](https://professorfeijo.com.br)
---
+++ src/App.jsx
@@ -0,0 +1,187 @@
+import {
+  getWebInstrumentations,
+  initializeFaro,
+  LogLevel,
+} from "@grafana/faro-web-sdk";
+import { TracingInstrumentation } from "@grafana/faro-web-tracing";
+import { PrimeReactProvider } from "primereact/api";
+import { Button } from "primereact/button";
+
+const appId = ""; // Your Grafana App Id
+const appName = ""; // Your application name
+
+let faro = initializeFaro({
+  url: `https://faro-collector-prod-sa-east-1.grafana.net/collect/${appId}`,
+  app: {
+    name: appName,
+    version: "1.0.0",
+    environment: "production",
+  },
+
+  instrumentations: [
+    // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
+    ...getWebInstrumentations(),
+
+    // Initialization of the tracing package.
+    // This packages is optional because it increases the bundle size noticeably. Only add it if you want tracing data.
+    new TracingInstrumentation(),
+  ],
+});
+
+const handleLog = () => {
+  faro.api.pushLog(
+    ["Hello world", new Date().getTime()],
+    { level: LogLevel.Debug },
+    { skipDedupe: true }
+  );
+};
+
+const handleInfo = () => {
+  faro.api.pushLog(["Hello world", 123], { level: LogLevel.INFo });
+};
+
+const handleWarn = () => {
+  faro.api.pushLog(["Hello world", 123], { level: LogLevel.WARN });
+};
+
+const handleError = () => {
+  faro.api.pushError(new Error("[FORM] Valor invalido de campo (nome)"), {
+    type: "form-validation",
+    context: {
+      message: "React error boundary",
+    },
+  });
+};
+
+const handleEvent = () => {
+  faro.api.pushEvent(
+    "evento-de-click",
+    { formulario: "cadastro-exemplo" },
+    null,
+    { skipDedupe: true }
+  );
+};
+
+const handleSetView = () => {
+  faro.api.setView({ name: "another-route" });
+};
+
+const handleSetUser = () => {
+  faro.api.resetUser();
+  faro.api.setUser({
+    email: "bob@example.com",
+    id: "123abc",
+    username: "bob",
+    attributes: {
+      role: "manager",
+    },
+  });
+};
+
+const App = () => {
+  const value = {
+    ripple: true,
+  };
+
+  return (
+    <PrimeReactProvider value={value}>
+      <div className="max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-4xl border-2 p-2 mt-4 bg-[#ffffff]">
+        <div className="grid grid-cols-12 grid-rows-10 gap-4">
+          <div className="col-span-12 flex align-middle justify-center">
+            <h1 className="text-3xl">Demo Grafana Faro - TDC</h1>
+          </div>
+          <div className="col-span-3 col-start-1 row-start-2 flex">
+            <Button
+              label="Set User"
+              icon="pi pi-user"
+              size="small"
+              className="w-full"
+              severity="secondary"
+              onClick={handleSetUser}
+            />
+          </div>
+          <div className="col-span-3 row-start-3 flex">
+            <Button
+              label="Log Default"
+              icon="pi pi-chevron-right"
+              size="small"
+              className="w-full"
+              onClick={handleLog}
+            />
+          </div>
+          <div className="col-span-3 col-start-1 row-start-4 flex">
+            <Button
+              label="Log de Info"
+              icon="pi pi-chevron-right"
+              size="small"
+              className="w-full"
+              severity="info"
+              onClick={handleInfo}
+            />
+          </div>
+          <div className="col-span-3 col-start-1 row-start-5 flex">
+            <Button
+              label="Log de Warning"
+              icon="pi pi-chevron-right"
+              size="small"
+              className="w-full"
+              severity="warning"
+              onClick={handleWarn}
+            />
+          </div>
+          <div className="col-span-3 col-start-1 row-start-6 flex">
+            <Button
+              label="Error"
+              icon="pi pi-chevron-right"
+              size="small"
+              className="w-full"
+              severity="danger"
+              onClick={handleError}
+            />
+          </div>
+          <div className="col-span-3 col-start-1 row-start-7 flex">
+            <Button
+              label="Change View"
+              icon="pi pi-chevron-right"
+              size="small"
+              className="w-full"
+              severity="secondary"
+              onClick={handleSetView}
+            />
+          </div>
+          <div className="col-span-3 col-start-1 row-start-8 flex">
+            <Button
+              label="Evento de clique"
+              icon="pi pi-chevron-right"
+              size="small"
+              className="w-full"
+              severity="secondary"
+              onClick={handleEvent}
+            />
+          </div>
+          <div className="col-span-9 row-span-2 col-start-5 row-start-2">
+            Para fazer uso desse projeto corretamente, crie uma conta em{" "}
+            <a
+              className="text-orange-500"
+              href="https://grafana.com/auth/sign-in/"
+              target="_blank"
+            >
+              Grafana Cloud
+            </a>{" "}
+            e siga os passos do{" "}
+            <a
+              className="text-orange-500"
+              href="https://github.com/csfeijo/grafana-faro-demo/blob/main/README.md"
+              target="_blank"
+            >
+              README.md
+            </a>{" "}
+            desse projeto.
+          </div>
+        </div>
+      </div>
+    </PrimeReactProvider>
+  );
+};
+
+export default App;
